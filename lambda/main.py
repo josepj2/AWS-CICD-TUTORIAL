7a918333c7f57f9a7b3199aac5dfb77495c5b11b
Jason Joseph <josepj2@gmail.com>
2025-11-26 19:34:20 +0530

    added dynamo db integration

--- lambda/main.py
+++ lambda/main.py
@@ -1,9 +1,33 @@
+import os
+import boto3
+
+
 def handler(event, context):
+    path  = event['rawPath']
+    if path != '/':
+        return {
+            'statusCode': 404,
+            'body': 'Not Found'
+        }   
+
+    dynamodb = boto3.resource('dynamodb')
+    table = os.environ.get('TABLE_NAME')
+    
+    response = table.get_item(Key = {'key': 'visit_count'})
+    if 'Item' in response:
+        visit_count = response['Item']['value']
+
+    else:
+        visit_count = 0
+
+    new_visit_count = visit_count + 1
+    table.put_item(Item = {'key': 'visit_count', 'value': new_visit_count})
 
     response_body = {
 
-        'message': 'Hello, World!',
-        'version': '1.0.0'
+        'message': 'Hello, World! ',
+        'version': '1.0.0',
+        'visit_count': new_visit_count
     }
     return {
         'statusCode': 200,
--- lib/aws-cicd-tutorial-stack.ts
+++ lib/aws-cicd-tutorial-stack.ts
@@ -2,6 +2,9 @@ import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as dotenv from 'dotenv';
+import { dot } from 'node:test/reporters';
 
 export class AwsCicdTutorialStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,14 +16,24 @@ export class AwsCicdTutorialStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'AwsCicdTutorialQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    dotenv.config();
+    const table = new dynamodb.Table(this, 'VisitedCountTable', {
+      partitionKey: { name: 'key', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
 
     const lambdaFunction = new lambda.Function(this , 'LambdaFunction', {
       runtime: lambda.Runtime.PYTHON_3_9,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'main.handler',
-
+      environment: {
+        TABLE_NAME: table.tableName,
+        VERSION: process.env.VERSION || "dev"
+      }
+      
     });
-
+    table.grantReadWriteData(lambdaFunction);
+    
     const functionUrl = lambdaFunction.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
       cors: {
